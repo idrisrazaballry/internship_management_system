8291f88a8e30b190e90951e3f1921ef08596412c
Idrisraza Ballary <152577081+idrisrazaballry@users.noreply.github.com>
2026-02-28 22:16:17 +0530

    internship.sql

--- internship.sql
+++ internship.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 12, 2019 at 08:38 PM
+-- Generation Time: Sep 12, 2024 at 08:38 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
